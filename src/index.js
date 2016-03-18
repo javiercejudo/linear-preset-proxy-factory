e817024d653dbe7693765b46e8fa6453cbe0b0ac
Javier Cejudo <javier@javiercejudo.com>
2016-03-19 02:08:50 +1100

    refactor: simplify code

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ const anyToAnyFactory = require('linear-preset-any-to-any');
 
 module.exports = (Decimal, conversions) => {
   const anyToAny = anyToAnyFactory(Decimal);
+  const get = (target, name) => anyToAny(target, ...name.split('_'));
 
-  return new Proxy(conversions, {
-    get: (target, name) => {
-      return anyToAny(target, ...name.split('_'));
-    }
-  });
+  return new Proxy(conversions, { get });
 };
