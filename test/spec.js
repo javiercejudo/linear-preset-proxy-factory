290ae9650b7f2173501b0b3d210de625138c0e86
Javier Cejudo <javier@javiercejudo.com>
2016-04-27 21:18:54 +1000

    test: node 6 supports proxies!

--- test/spec.js
+++ test/spec.js
@@ -5,16 +5,20 @@
 require('should');
 
 const Decimal = require('arbitrary-precision')(require('floating-adapter'));
+const presetToNumber = require('linear-preset-to-number');
 const proxyFactory = require('../src/');
 
 describe('linear preset factory', () => {
-  it.skip('should create linear presets', () => {
+  it('should create linear presets', () => {
     const proxiedConversions = proxyFactory(Decimal, {
         a: [[0, 1], [0, 1]],
         b: [[1, 2], [3, 4]],
         c: [[0, -5], [0, 30]]
     });
 
-    proxiedConversions.c_b.should.eql([[0, 30], [2, -3]]);
+    Object.prototype.hasOwnProperty.call(proxiedConversions, 'c_b')
+      .should.be.exactly(false);
+
+    presetToNumber(proxiedConversions.c_b).should.eql([[0, 30], [2, -3]]);
   });
 });
